755a491ea5b88a2cdb3196f296878352c4073a22
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-08-05 11:45:09 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -5,8 +5,7 @@ resource "aws_servicecatalog_portfolio" "portfolio" {
   name          = "My App Portfolio"
   description   = "List of my organizations apps"
   provider_name = "Brett"
-
-
+}
 resource "aws_servicecatalog_portfolio_share" "example" {
   principal_id = "390132021439"
   portfolio_id = aws_servicecatalog_portfolio.portfolio.id
