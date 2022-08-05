66625d967b040bd8b400f47b1152b7aab68aaff1
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-08-05 11:42:16 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
 resource "aws_servicecatalog_portfolio_share" "example" {
   principal_id = "390132021439"
   portfolio_id = aws_servicecatalog_portfolio.example.id
