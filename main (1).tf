c21394c9c892d682b20bc374c1b431d6d4c57ba4
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-08-09 11:01:53 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,13 +1,33 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_servicecatalog_portfolio" "portfolio" {
-  name          = "My App Portfolio"
-  description   = "List of my organizations apps"
-  provider_name = "Brett"
-}
-resource "aws_servicecatalog_portfolio_share" "example" {
-  principal_id = "390132021439"
-  portfolio_id = aws_servicecatalog_portfolio.portfolio.id
-  type         = "ACCOUNT"
+
+resource "aws_s3_bucket" "source" {
+
+  bucket   = "tf-test-bucketnidhi"
+ }
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Statement" = [
+      {
+        "Action" = [
+          "s3:GetAccessPoint",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketAcl",
+          "s3:GetBucketLocation",
+          "s3:GetBucketLogging"
+          ]
+        "Effect"   = "Allow"
+       "Resource": [
+        "${aws_s3_bucket.source.arn}"
+       ]
+      },
+    ]
+  })
 }
